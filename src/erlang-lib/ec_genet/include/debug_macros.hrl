bd2a4f2b02ad70f8ba60ad8a8c3ed9de52540e72
Martin Volf <mvolf@cisco.com>
2017-02-06 17:54:27 +0100

    LOGMSG is now debug, not trace

--- src/erlang-lib/ec_genet/include/debug_macros.hrl
+++ src/erlang-lib/ec_genet/include/debug_macros.hrl
@@ -28,7 +28,8 @@
 -define(LOG(Level,Msg, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6),
         ec_genet_logger:log(Level, ?MODULE, ?LINE,
                                     ["LOG ~s~n ~s=~p~n ~s=~p~n ~s=~p~n ~s=~p~n ~s=~p~n ~s=~p",
-                                     [Msg, ??Arg1, Arg1, ??Arg2, Arg2, ??Arg3, Arg3, ??Arg4, Arg4, ??Arg5, Arg5, ??Arg6, Arg6]])).
+                                     [Msg, ??Arg1, Arg1, ??Arg2, Arg2, ??Arg3, Arg3, ??Arg4, Arg4,
+                                      ??Arg5, Arg5, ??Arg6, Arg6]])).
 
 -define(LOGERR(Msg), ?LOG(error, Msg)).
 -define(LOGERR(Msg, Arg1), ?LOG(error, Msg, Arg1)).
@@ -62,21 +63,21 @@
 -define(LOGINFO(Msg, Arg1, Arg2, Arg3, Arg4, Arg5), ?LOG(info, Msg, Arg1, Arg2, Arg3, Arg4, Arg5)).
 -define(LOGINFO(Msg, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6), ?LOG(info, Msg, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)).
 
--define(LOGMSG(Msg), ?LOG(trace, Msg)).
--define(LOGMSG(Msg, Arg1), ?LOG(trace, Msg, Arg1)).
--define(LOGMSG(Msg, Arg1, Arg2), ?LOG(trace, Msg, Arg1, Arg2)).
--define(LOGMSG(Msg, Arg1, Arg2, Arg3), ?LOG(trace, Msg, Arg1, Arg2, Arg3)).
--define(LOGMSG(Msg, Arg1, Arg2, Arg3, Arg4), ?LOG(trace, Msg, Arg1, Arg2, Arg3, Arg4)).
--define(LOGMSG(Msg, Arg1, Arg2, Arg3, Arg4, Arg5), ?LOG(trace, Msg, Arg1, Arg2, Arg3, Arg4, Arg5)).
--define(LOGMSG(Msg, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6), ?LOG(trace, Msg, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)).
+-define(LOGMSG(Msg), ?LOG(debug, Msg)).
+-define(LOGMSG(Msg, Arg1), ?LOG(debug, Msg, Arg1)).
+-define(LOGMSG(Msg, Arg1, Arg2), ?LOG(debug, Msg, Arg1, Arg2)).
+-define(LOGMSG(Msg, Arg1, Arg2, Arg3), ?LOG(debug, Msg, Arg1, Arg2, Arg3)).
+-define(LOGMSG(Msg, Arg1, Arg2, Arg3, Arg4), ?LOG(debug, Msg, Arg1, Arg2, Arg3, Arg4)).
+-define(LOGMSG(Msg, Arg1, Arg2, Arg3, Arg4, Arg5), ?LOG(debug, Msg, Arg1, Arg2, Arg3, Arg4, Arg5)).
+-define(LOGMSG(Msg, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6), ?LOG(debug, Msg, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -define(ASSERT(Expected,Actual),
-	case Actual of
-		Expected -> ok;
-		_ -> io:format("~nASSERTION FAILED ~s:~p~nExpected=~p~n  Actual=~p~n", 
-                  [?MODULE, ?LINE, Expected, Actual]), Expected=Actual
-	end).
+        case Actual of
+            Expected -> ok;
+            _ -> io:format("~nASSERTION FAILED ~s:~p~nExpected=~p~n  Actual=~p~n",
+                           [?MODULE, ?LINE, Expected, Actual]), Expected=Actual
+        end).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
